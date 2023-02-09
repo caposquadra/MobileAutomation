872dbbefb86564e541cf9d9ee2ce1115ff6903de
Dmitrii Zimin <dmitryyzimin@gmail.com>
2023-02-09 22:34:11 +0300

    refactoring for #3, #5, and #6 exercies

--- src/Ex6.java
+++ src/Ex6.java
@@ -1,98 +1,20 @@
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.net.URL;
 
-public class Ex6 {
-
-
-    private AppiumDriver driver;
-
-    @Before
-    public void setup() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("platformVersion", "8.0.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Users\\dmitr\\OneDrive\\Desktop\\Автоматизация МП\\02 Установка и настройка инструментов\\04. Архив с утилитами\\Tools\\org.wikipedia.apk");
-
-        String spec;
-        driver = new AndroidDriver(new URL(spec = "http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+public class Ex6 extends CoreTestCase {
 
     @Test
     public void testArticlesHasTitle() {
 
         String search_query_1 = "Java";
-        String article_1_title = "Java (programming language)";
-        String article_1_subtitle = "oriented programming language";
 
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_container"),
-                "Couldn't find Search input",
-                20
-        );
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine(search_query_1);
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
 
-        waitForElementAndSendKeys(
-                By.id("org.wikipedia:id/search_container"),
-                search_query_1,
-                "Couldn't find Search field to input the query",
-                20
-        );
-
-        waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text, '" + article_1_subtitle + "')]"),
-                "Couldn't find + '" + article_1_subtitle + "' + topic searching by search_line",
-                20
-        );
-
-        assertElementPresent(
-                By.id("org.wikipedia:id/view_page_title_text")
-        );
+        ArticlePageObject.assertElementPresent();
     }
-
-    private void assertElementPresent (By by) {
-        WebElement element = driver.findElement(by);
-        Assert.assertTrue("There is no an article title element on the page", element != null);
-        }
-
-    private WebElement waitForElementPresent(By by, String error_message, long timeOutInSeconds) {
-        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
-        wait.withMessage(error_message + "\n");
-        return wait.until(
-                ExpectedConditions.presenceOfElementLocated(by)
-        );
-    }
-
-    private WebElement waitForElementAndClick(By by, String error_message, long timeOutInSeconds) {
-        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
-        element.click();
-        return element;
-    }
-
-    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutInSeconds) {
-        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
-        element.sendKeys(value);
-        return element;
-    }
-
-
-
 }
